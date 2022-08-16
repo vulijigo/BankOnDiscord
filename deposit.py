545bcadd7b1a8fc77dceec4217c9495b8ca90135
Navraj Singh <navraj@outlook.com>
2022-08-16 21:05:14 +1000

    Import API call implemented

--- deposit.py
+++ deposit.py
@@ -3,16 +3,19 @@ import hikari
 import requests
 from constants import baseUrl
 import os
+import json
 async def Deposit(wallet, event: hikari.DMMessageCreateEvent):
-    checkWalletUrl = baseUrl + 'wallets/' + str(wallet) + '?contents=false'
+    checkWalletUrl = baseUrl + 'wallets/' + str(event.author_id) + '?contents=false'
     response = requests.get(checkWalletUrl)
     responsejson = response.json()
-    if(responsejson['payload']['message'] == 'Wallet not found'):
+    
+    if(responsejson['status'] != 'success'):
         print('Wallet does not exist.Creating new one')
         createWalletUrl = baseUrl + 'wallets'
-        walletJson = { 'name': str(wallet)}
+        walletJson = { 'name': str(event.author_id)}
         createresponse = requests.post(createWalletUrl, json= walletJson)
         createresponsejson = createresponse.json()
+        print(createresponsejson)
         if(createresponsejson['status'] == 'success'):
             print('Wallet Created successfully')
     print('Depositing', len(event.message.attachments), ' files')
@@ -21,15 +24,18 @@ async def Deposit(wallet, event: hikari.DMMessageCreateEvent):
     else:
         for coin in event.message.attachments:
             fdata = await coin.read()
-            filename = os.path.join(os.getcwd() + '\\import',coin.filename)
+            filename = os.path.join(os.getcwd() + os.sep + 'import',coin.filename)
             print(filename)
             with open(filename, "wb") as binary_file:
                 binary_file.write(fdata)
             depositUrl = baseUrl + 'import'
-            depositJson = {'name': str(wallet), 'items':[{'type':'file', 'data':filename}]}
-            print(depositJson)
-            depositresponse = requests.post(depositUrl, depositJson)
+            depositJson = {"name": str(event.author_id), "items":[{"type":"file", "data":filename}]}
+            json_string = json.dumps(depositJson) 
+            print(json_string)
+            depositresponse = requests.post(depositUrl, json_string)
             depositresponsejson = depositresponse.json()
+            if(depositresponsejson['status'] == 'success'):
+                await event.message.respond('Coins imported..')
             print(depositresponsejson)
 
     return ''
